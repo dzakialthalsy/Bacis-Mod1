f38365af62dec74486edb4f6596fc10a11117706
dzakialthalsy <74495697+dzakialthalsy@users.noreply.github.com>
2025-01-22 23:36:12 +0700

    Add files via upload

---
+++ MOD.java
@@ -0,0 +1,12 @@
+class coba {
+    public static void main(String[] args) {
+        int[] x = {128, 322, 353, 235, 336, 73, 198, 332, 202, 285, 57, 87, 262, 221, 
+            218, 405, 335, 101, 256, 227, 112, 140};
+
+        String a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+
+        for (int i : x) {
+            System.out.print(a.charAt(i % 37));
+        }
+    }
+}
