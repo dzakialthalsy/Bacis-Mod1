8aead86429df45d01e44c2612bb3b41adc51d94c
dzakialthalsy <74495697+dzakialthalsy@users.noreply.github.com>
2025-01-22 23:40:38 +0700

    Update MOD.java

--- MOD.java
+++ MOD.java
@@ -1,4 +1,4 @@
-class coba {
+class MOD {
     public static void main(String[] args) {
         int[] x = {128, 322, 353, 235, 336, 73, 198, 332, 202, 285, 57, 87, 262, 221, 
             218, 405, 335, 101, 256, 227, 112, 140};
